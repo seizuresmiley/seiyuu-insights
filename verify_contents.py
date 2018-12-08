2c91779f583e76a157dc052b319dfffc73c9c924
seizuresmiley <shoopedlol@gmail.com>
2018-12-08 15:16:30 +0700

    fix verify tool working directory and count.

--- verify_contents.py
+++ verify_contents.py
@@ -2,8 +2,9 @@ import os
 import tweepy
 import json
 import csv
-#sets the working directory.
-working_dir = "result_raw_small/"
+# sets the working directory.
+working_dir = "result_raw/"
+
 
 def filelist():
     '''gets all files in the result_raw directory'''
@@ -13,34 +14,39 @@ def filelist():
         files_list.append(filename)
         return files_list
 
+
 def user_exists():
     '''checks for incomplete data collection'''
+    # gets global variable
     global working_dir
+    # opens the list.csv file.
+    with open('list.csv', mode='r') as target_list:
+        target_dict = csv.DictReader(target_list)
         files_list = filelist()
+        # collects all the mismatched users.
         mismatches = []
         for row in target_dict:
             seiyuu_name = row['name']
             target_user = row['handle']
-            if row['handle'] == "<none>" :
+            if row['handle'] == "<none>":
                 print("%s has no handle." % (row['name']))
             else:
-                single_result = {}
                 data = {}
-                with open("%s%s(%s).json" % (working_dir,target_user,seiyuu_name),"r",encoding="utf8") as proc:
+                # Loads the json file.
+                with open("%s%s(%s).json" % (working_dir, target_user, seiyuu_name), "r", encoding="utf8") as proc:
                     data = json.load(proc)
-                    print("Verifying",data['handle'])
+                    print("Verifying", data['handle'])
+                    # We check if the user has more than 3200 tweets and that we collected 3200
+                    # tweets.
                     if data['tweets'][0]['user']['statuses_count'] > 20 and len(data['tweets']) < 20:
                         mismatches.append(data['handle'])
-    if len(mismatches) > 0 :
+    # Outputs the results
+    if len(mismatches) > 0:
         print("Mismatches detected: ")
         for handle in mismatches:
             print(handle)
     else:
         print("No mismatches detected.")
 
-def single_user():
-    with open("result_raw/@kanako_nomura(Kanako Nomura).json","r+",encoding="utf8") as singleuser:
-        data = json.load(singleuser)
-        print(data["tweets"][0]["text"])
 
 user_exists()
