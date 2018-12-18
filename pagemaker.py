d253c63ec3c4048b32400c15d197a9434e9632fe
seizuresmiley <shoopedlol@gmail.com>
2018-12-18 13:01:49 +0700

    add comments to pagemaker

--- pagemaker.py
+++ pagemaker.py
@@ -2,8 +2,10 @@ import json
 import os
 
 def seiyuu_info(directory):
+    '''traverses the result directory'''
     for file in os.listdir(directory):
         with open("%s%s" % (directory,file), "r",encoding="utf8") as raw:
+            #we return the info for the text replacement
             info = json.load(raw)
             handle = info['handle']
             real_name = info['name']
@@ -13,10 +15,16 @@ def seiyuu_info(directory):
             
         
 def makepage(handle,handle_no_at,real_name):
+    '''replaces text from template.html'''
+    #opens the result file
     with open('analysis_result/pages/%s.html' % (handle_no_at), "w+",encoding='utf8') as resultpage:
+        #opens the template file
         with open('template.html',"r",encoding="utf8") as template:
+            #traverses through each line
             for line in template:
+                #replaces the keywords with data from another function.
                 newline = line.replace("realname",real_name).replace("ins_handle",handle).replace("no_at",handle_no_at)
+                #writes it
                 resultpage.write(newline + "\n")
         template.close()
     resultpage.close()
